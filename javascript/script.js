5a8b8c2ea2e63c53ffb01739eb24fb628bcf04a2
AngeloResplandes <angeloodrigues@gmail.com>
2025-06-06 20:18:47 -0300

    novo commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,9 @@
-const value = document.querySelector("#value")
-const bank = document.querySelector("#bank")
-const months = document.querySelector("#months")
-const sumulator = document.querySelector("#simulator")
+const value = document.querySelector("#value") // input do valor total
+const bank = document.querySelector("#bank") // select com os bancos
+const months = document.querySelector("#months") //select com as quantidades de meses
+const sumulator = document.querySelector("#simulator") //botão para rodar a função o formatarCampo()
 
-const valueInstallment = document.querySelector(".value-installment")
-const valueTotal = document.querySelector(".value-total")
-const installments = document.querySelector(".installments")
-const btnSimulateAgain = document.querySelector(".btn-simulate-again")
-
+const valueInstallment = document.querySelector(".value-installment") // campo para receber o valor das parcelas
+const valueTotal = document.querySelector(".value-total") // campo para receber o valor final
+const installments = document.querySelector(".installments") // campo das quantidades de parcelas
+const btnSimulateAgain = document.querySelector(".btn-simulate-again") //botão para voltar a pagina inicial
