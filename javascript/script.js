e7a5690cdd541a0f822915ae65ac6607ca564395
AngeloResplandes <angeloodrigues@gmail.com>
2025-06-06 17:51:36 -0300

    add javascript variables

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+const value = document.querySelector("#value")
+const bank = document.querySelector("#bank")
+const months = document.querySelector("#months")
+const sumulator = document.querySelector("#simulator")
+
+const valueInstallment = document.querySelector(".value-installment")
+const valueTotal = document.querySelector(".value-total")
+const installments = document.querySelector(".installments")
+const btnSimulateAgain = document.querySelector(".btn-simulate-again")
+
